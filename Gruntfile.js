18edb7162ddc04a59e9a61e3228d533c652df945
unknown <suhair@ISPG391.server3.ispg.in>
2013-08-14 11:00:24 +0530

    grunt-contrib-connect is installed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function(grunt) {
+
   grunt.registerTask("default", "A default task", function(){
   	grunt.log.writeln("Hello Grunt!");
   });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
 };
