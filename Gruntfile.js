c9ea8f7df923f755c2067304a6ea18322d9dafac
unknown <suhair@ISPG391.server3.ispg.in>
2013-08-14 11:36:07 +0530

    Grunt watch plugin installed and runserver custom task created

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,22 @@
 module.exports = function(grunt) {
 
+  grunt.initConfig({
+    connect: {
+      uses_defaults: {}
+    },
+    watch:{
+    	
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask("default", "A default task", function(){
   	grunt.log.writeln("Hello Grunt!");
   });
 
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask("runserver", ["connect", "watch"]);
+
+  
 };
